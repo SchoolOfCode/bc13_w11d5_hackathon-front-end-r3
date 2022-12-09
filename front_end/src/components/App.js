c396f49a71cacaedba45ba0b8f3ee41c962288c6
mvhmz81 <mvhmz@yahoo.co.uk>
2022-12-09 16:29:49 +0000

    added backend env

--- front_end/src/components/App.js
+++ front_end/src/components/App.js
@@ -1,13 +1,14 @@
 import { useState, createContext, useEffect } from "react";
 import "./App.css";
 import ListData from "../components/ListData/ListData";
+const URL = process.env.REACT_APP_URL;
 // import data1 from "..//data.json";
 export const dataContext = createContext();
 function App() {
   const [data, setData] = useState([]);
   useEffect(() => {
     async function getInitialData() {
-      let response = await fetch("http://localhost:8000/api/Dinosaurs");
+      let response = await fetch(`${URL}/api/Dinosaurs`);
       let data = await response.json();
       setData(data);
     }
