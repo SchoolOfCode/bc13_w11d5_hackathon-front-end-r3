f6bbc8974e246922d80e8f963a4035c839a8a1ee
tariba <tariqxa@gmail.com>
2022-12-09 11:45:35 +0000

    card component added

---
+++ front_end/src/components/Card/Card.js
@@ -0,0 +1,18 @@
+
+import "./Card.css"
+    function Card({ title, description, poster_path, buttonText1, buttonText2}){
+    return (
+        <div className="card_container">
+            <div className= "card">
+                <h3 >{title}</h3>
+                <p>{description}</p>
+                <img alt="soc" width={"100%"} src={poster_path}/>
+                    <div className="Button_Container">
+                      
+                    </div>
+            </div>
+            
+        </div>
+        )
+    }
+    export default Card;
