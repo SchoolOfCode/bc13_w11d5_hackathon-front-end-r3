99098fcc79f5c16f34b639711c0a3edc72f658ee
mvhmz81 <mvhmz@yahoo.co.uk>
2022-12-09 12:35:53 +0000

    struggle with getting a card for each dinosaur

--- front_end/src/components/ListData/Card/Card.js
+++ front_end/src/components/ListData/Card/Card.js
@@ -1,18 +1,20 @@
+import data from "../../../data.json";
 
-import "./Card.css"
-    function Card({ title, description, poster_path}){
-    return (
-        <div className="card_container">
-            <div className= "card">
-                <h3 >{title}</h3>
-                <p>{description}</p>
-                <img alt="soc" width={"100%"} src={poster_path}/>
-                    <div className="Button_Container">
-                      
-                    </div>
-            </div>
-            
-        </div>
-        )
-    }
-    export default Card;
+import "./Card.css";
+function Card() {
+  console.log(data);
+  return (
+    <div className="card_container">
+      <div className="card">
+        {data.map((data) => {
+          return <h3> {data.Name}</h3>;
+        })}
+        {/* <h3>{title}</h3>
+        <p>{description}</p>
+        <img alt="soc" width={"100%"} src={poster_path} /> 
+         <div className="Button_Container"></div> */}
+      </div>
+    </div>
+  );
+}
+export default Card;
